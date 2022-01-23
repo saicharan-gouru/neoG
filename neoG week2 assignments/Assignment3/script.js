9623fcbd621045755d3f750aa06afa6ada819072
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-01-23 22:38:33 +0530

    Update script.js

--- neoG week2 assignments/Assignment3/script.js
+++ neoG week2 assignments/Assignment3/script.js
@@ -1,1 +1,11 @@
 document.getElementById('auto-focus').focus();
+
+const child = document.querySelectorAll('.child');
+
+
+
+function Handler(eve) {
+    for (let i = 0; i < child.length; i++)
+        child[i].className = child[i].className.replace(" active", "");
+    eve.currentTarget.className += " active";
+}
