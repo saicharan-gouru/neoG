4a8753be883a0b05f0628e65460e35e442dd46dd
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-01-24 18:54:19 +0530

    Create script.js

---
+++ neoG week2 assignments/Assignment8/script.js
@@ -0,0 +1,8 @@
+child = document.querySelectorAll(".child");
+
+
+function Handler(eve) {
+    for (let i = 0; i < child.length; i++)
+        child[i].className = child[i].className.replace(" active", "");
+    eve.currentTarget.className += " active";
+}
