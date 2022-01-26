2f1a069056ca8e587c8ed4d88ce86e731e31c524
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-01-26 12:16:33 +0530

    Create script.js

---
+++ neoG week2 assignments/Assignment7/script.js
@@ -0,0 +1,26 @@
+const btnDialog = document.querySelector(".btn-dialog");
+const dialog = document.querySelector(".dialog");
+const close = document.querySelector(".far");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+btnDialog.addEventListener("click", () => {
+    dialog.style.display = "block";
+
+})
+
+close.addEventListener("click", () => {
+    dialog.style.display = "none";
+})
