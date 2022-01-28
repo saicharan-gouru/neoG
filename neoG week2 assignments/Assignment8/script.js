43c7d066794eef198b2be8ba036d15c16aff9892
Saicharan Gouru <gourusaicharan6@gmail.com>
2022-01-28 19:32:16 +0530

    Update script.js

--- neoG week2 assignments/Assignment8/script.js
+++ neoG week2 assignments/Assignment8/script.js
@@ -1,8 +1,14 @@
-child = document.querySelectorAll(".child");
-
+const child = document.querySelectorAll(".child");
+const Schild = document.querySelectorAll(".s-child");
 
 function Handler(eve) {
     for (let i = 0; i < child.length; i++)
         child[i].className = child[i].className.replace(" active", "");
     eve.currentTarget.className += " active";
+}
+
+function Shandler(eve) {
+    for (let i = 0; i < Schild.length; i++)
+        Schild[i].className = Schild[i].className.replace(" s-active", "");
+    eve.currentTarget.className += " s-active";
 }
